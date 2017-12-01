af273e1baf8b792eae3c04c9e5768f13056595cb
webmaster <gi@redrocketmedia.ru>
2017-12-01 20:14:21 +0300

    JDBC 04_sqldatasources v4. Added PGBaseDataSourceJNDI class. Need Java app server

--- JDBC/04_sqldatasources/src/main/java/oracle/sqldatasources/PGBaseDataSourceJNDI.java
+++ JDBC/04_sqldatasources/src/main/java/oracle/sqldatasources/PGBaseDataSourceJNDI.java
@@ -7,39 +7,46 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.LinkedList;
-import org.postgresql.ds.PGSimpleDataSource;
+import java.util.Properties;
+//import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import org.postgresql.ds.PGPoolingDataSource;
 /**
- * Класс PGBaseDataSource демонстрирует работу с объектом DataSource
- * базовой реализации без использования JNDI на примере СУБД PostgreSQL.
+ * Класс PGBaseDataSourceJNDI демонстрирует работу с объектом DataSource
+ * базовой реализации с использованием JNDI на примере СУБД PostgreSQL.
+ * !!! Для запуска необходим сервер приложений JAVA !!!
  *
  * @see https://docs.oracle.com/javase/tutorial/jdbc/basics/sqldatasources.html
  * @see https://jdbc.postgresql.org/documentation/head/ds-ds.html
  * @author Gureyev Ilya (mailto:ill-jah@yandex.ru)
  * @version 1
- * @since 2017-11-30
+ * @since 2017-12-01
  */
-class PGBaseDataSource {
+class PGBaseDataSourceJNDI {
     /**
      * Соединение с бд.
      */
 	private Connection con;
-    /**
-     * DataSource.
-     */
-    private final PGSimpleDataSource ds;
     /**
      * Конструктор. Устанавливает свойства.
      * @param server url сервера (например localhost).
      * @param db имя базы данных.
      * @param user пользователь СУБД.
      * @param pass пароль пользователя СУБД.
+     * @throws NamingException ошибка JNDI.
      */
-    PGBaseDataSource(String server, String db, String user, String pass) {
-        this.ds = new PGSimpleDataSource();
-        this.ds.setServerName(server);
-        this.ds.setDatabaseName(db);
-        this.ds.setUser(user);
-        this.ds.setPassword(pass);
+    PGBaseDataSourceJNDI(String server, String db, String user, String pass) throws NamingException {
+        PGPoolingDataSource ds = new PGPoolingDataSource();
+        ds.setDataSourceName("A Data Source");
+        ds.setServerName(server);
+        ds.setDatabaseName(db);
+        ds.setUser(user);
+        ds.setPassword(pass);
+        Properties props = new Properties();
+        props.setProperty();
+        new InitialContext(props).rebind("DataSource", ds);
     }
     /**
      * Закрывает соединение с СУБД.
@@ -63,8 +70,9 @@ class PGBaseDataSource {
      * @param query sql-запрос.
      * @return список с результатом запроса к бд.
      * @throws SQLException ошибка SQL.
+     * @throws NamingException ошибка JNDI.
      */
-    public LinkedList<HashMap<String, String>> select(String query) throws SQLException {
+    public LinkedList<HashMap<String, String>> select(String query) throws SQLException, NamingException {
     	if (this.con == null) {
 			this.setConnection();
     	}
@@ -88,8 +96,10 @@ class PGBaseDataSource {
     /**
      * Устанавливает соединение с СУБД.
      * @throws SQLException ошибка SQL.
+     * @throws NamingException ошибка JNDI.
      */
-    public void setConnection() throws SQLException {
-        this.con = this.ds.getConnection();
+    public void setConnection() throws SQLException, NamingException {
+        DataSource source = (DataSource) new InitialContext().lookup("DataSource");
+        this.con = source.getConnection();
     }
 }
